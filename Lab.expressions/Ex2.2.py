97bd970afaeda6cae4fd7dfb7d15c81e9f82b59c
D. Port Me <dport@hawaii.edu>
2025-01-28 13:15:36 -1000

    xxx

---
+++ Lab.expressions/Ex2.2.py
@@ -0,0 +1,3 @@
+birth_year = input("What year were you born (Ex. 2021)? ")
+age = 2025 - int(birth_year)
+print(f"You were born in {birth_year} and you are {age} years old.")
---
+++ Lab.expressions/Ex2.4.py
@@ -0,0 +1,7 @@
+user_input = input("Please enter a decimal number between 1 and 100: ")
+# convert input to a number and compute its square
+number = float(user_input)
+squared_number = number ** 2
+
+print(f"You entered the number: {number}")
+print(f"The square of {number} is: {squared_number}")
