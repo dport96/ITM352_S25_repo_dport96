3cd8aae9eabeb0bd25c5aa21d86060f0f0e9c6ec
D. Port Me <dport@hawaii.edu>
2025-01-30 13:32:41 -1000

    Update Ex2.3.py

--- Lab.expressions/Ex2.3.py
+++ Lab.expressions/Ex2.3.py
@@ -1,7 +1,7 @@
 user_input = input("Please enter a decimal number between 1 and 100: ")
 # convert input to a number and compute its square
 number = float(user_input)
-squared_number = number ** 2
+squared_number = Math.pow()
 
 print(f"You entered the number: {number}")
 print(f"The square of {number} is: {squared_number}")
