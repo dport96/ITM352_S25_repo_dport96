6c5afe2c561b57985842670a71a24e6fa828a74c
D. Port Me <dport@hawaii.edu>
2025-02-04 13:16:52 -1000

    xxx

---
+++ Lab.Functions/Ex1a.py
@@ -0,0 +1,12 @@
+from cryptography.fernet import Fernet
+
+message = "hello 352ers!"
+
+key = Fernet.generate_key()
+
+fernet = Fernet(key)
+
+encMessage = fernet.encrypt(message.encode())
+
+print("original string: ", message)
+print("encrypted string: ", encMessage)
