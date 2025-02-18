551d381f827b76632956a4b8600fa282533a1c19
D. Port Me <dport@hawaii.edu>
2025-02-18 13:16:26 -1000

    xxx

--- Lab.Dict/Ex2c.py
+++ Lab.Dict/Ex2c.py
@@ -3,13 +3,11 @@ Write Python code to define a list of taxi trip durations in miles (use values 1
 """
 trip_durations = [1.1, 0.8, 2.5, 2.6]
 trip_fares = (6.25, 5.25, 10.50, 8.05)
-trips = {
-    'miles' : [1.1, 0.8, 2.5, 2.6],
-    'fares': (6.25, 5.25, 10.50, 8.05)
-}
-trips['miles'].append(2.2)
-trips['fares'] += (1.23,)
-trips['miles'][0] = 2.2
+trips = list(zip(trip_durations,trip_fares))
+print(trips)
+
+"""
 trip_num = int(input('What trip do you want:')) 
 print(f"Duration: {trips['miles'][trip_num - 1]} hours")
 print(f"Cost: ${trips['fares'][trip_num - 1]}")
+"""
---
+++ Lab.Dict/Ex2d.py
@@ -0,0 +1,3 @@
+trip_fares = [(1.1, 6.25), (0.8, 5.25), (2.5, 10.5), (2.6, 8.05)]
+print(f"Duration: {trip_fares[2][0]} hours")
+print(f"Cost: ${trip_fares[2][1]}")
